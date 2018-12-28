73b2b771aa0f975b07e8d13b3df477537f262855
Keegan Rankin <kms.rankin@gmail.com>
2018-12-28 16:39:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,18 +21,3 @@ function currentLine(line) {
     array.push(i + 1 + ". " + line[i])
   } return ("The line is currently: " + array.join(', '))
 }
-  // while (i >= 0 && line[i] === "true") {
-  //   array.push(`${line[i]})`
-  //   return(`The line is currently: ${array}`);
-  //   i++
-  // } return "The line is currently empty."
-  // }
-
-//   var i = 0; i++;
-//
-//   if (line.length === 0) {
-//     return "The line is currently empty."
-//   } else {
-//     return `The line is currently ${[i + 1]. line[i]}.` // unsure how to number the array
-//   }
-// }
