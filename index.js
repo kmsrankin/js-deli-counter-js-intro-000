cba291edbff1b0de4ba2debad9997a334dc14b0a
Keegan Rankin <kms.rankin@gmail.com>
2018-12-28 16:39:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
   var array = []
   for (var i = 0; i < line.length; i++) {
     array.push(i + 1 + ". " + line[i])
-  } return ("The line is currently: " + array.join(, ))
+  } return ("The line is currently: " + array.join(', '))
 }
   // while (i >= 0 && line[i] === "true") {
   //   array.push(`${line[i]})`
