f8cc475ac6345b6d7c8b899be529000eff06556b
Keegan Rankin <kms.rankin@gmail.com>
2018-12-28 15:59:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
 function currentLine(line) {
   var i = 0
   while (line.length > 0) {
-    return `The line is currently ${line[i]}.`;
+    return `The line is currently ${line.length} ${line[i]}.`;
     i++;
   } return "The line is currently empty."
   }
