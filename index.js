cffa21d421332c589dd6733eff489f994a64be18
Keegan Rankin <kms.rankin@gmail.com>
2018-12-28 15:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
-  var i = 0; i++;
-  if (line.length === 0) {
-    return "The line is currently empty."
-  } else {
-    return `The line is currently ${[i + 1]. line[i]}.` // unsure how to number the array
+  while (line.length > 0) {
+    return `The line is currently ${line}.`
+  } return "The line is currently empty."
   }
-}
+
+//   var i = 0; i++;
+//   
+//   if (line.length === 0) {
+//     return "The line is currently empty."
+//   } else {
+//     return `The line is currently ${[i + 1]. line[i]}.` // unsure how to number the array
+//   }
+// }
